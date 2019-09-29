1af114e795ba59c29ad9c5dadfbbd4aa49dbb6ab
Alexander Rosa <alex.j.rosa@gmail.com>
2019-09-29 16:33:46 -0600

    1.13*: anecdotes step2 - using votes array

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -1,21 +1,6 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
-const App = ({ anecdotes }) => {
-  const [selected, setSelected] = useState(0);
-
-  const handleRandomAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
-
-  return (
-    <div>
-      {anecdotes[selected]} <br />
-      <Button onClick={handleRandomAnecdote} text="next anecdote" />
-    </div>
-  )
-}
-
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -24,6 +9,32 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
+const initialVotesArray = anecdotes.map(() => 0);
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const ButtonVote = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+
+const App = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0);
+  const [votesArray, setVote] = useState(initialVotesArray);
+
+  const handleRandomAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
+  const handleVote = (selected) => () => {
+    const voteCopyArray = [...votesArray]
+    voteCopyArray[selected] += 1;
+    setVote(voteCopyArray)
+  }
+
+  return (
+    <div>
+      {anecdotes[selected]} <br />
+      <p>has {votesArray[selected] || 0} votes </p>
+      <ButtonVote onClick={handleVote(selected)} text="vote" />
+      <Button onClick={handleRandomAnecdote} text="next anecdote" />
+    </div>
+  )
+}
+
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
