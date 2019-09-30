f589febfc9d990d797b72d030679577169e3fbf9
Alexander Rosa <alex.j.rosa@gmail.com>
2019-09-29 19:06:00 -0600

    1.14*: anecdotes step3 - Most Voted Anecdote Comp

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -8,29 +8,39 @@ const anecdotes = [
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+];
 const initialVotesArray = anecdotes.map(() => 0);
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 const ButtonVote = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const MostVotedAnecdote = ({ getHighestVoteIndex }) => {
+  return (
+    <div>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[getHighestVoteIndex]}</p>
+    </div>
+  )
+}
 
 const App = ({ anecdotes }) => {
   const [selected, setSelected] = useState(0);
   const [votesArray, setVote] = useState(initialVotesArray);
 
-  const handleRandomAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length))
-  const handleVote = (selected) => () => setVote(votesArray.map((voteItem, index) => (index === selected) ? voteItem += 1 : voteItem))
+  const handleRandomAnecdote = () => setSelected(Math.floor(Math.random() * anecdotes.length));
+  const handleVote = (selected) => () => setVote(votesArray.map((voteItem, index) => (index === selected) ? voteItem += 1 : voteItem));
+  const getHighestVoteIndex = votesArray.indexOf(Math.max(...votesArray));
 
   return (
     <div>
+      <h1>Anecdote of the Day</h1>
       {anecdotes[selected]} <br />
       <p>has {votesArray[selected] || 0} votes </p>
       <ButtonVote onClick={handleVote(selected)} text="vote" />
       <Button onClick={handleRandomAnecdote} text="next anecdote" />
+      <MostVotedAnecdote getHighestVoteIndex={getHighestVoteIndex} />
     </div>
   )
 }
-
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
